eda0b737e6a2911392f585516719c95ed14ccec8
rayfel <rayfelogando@gmail.com>
2025-02-07 00:31:05 -0400

    Ignorado en la serialización JSON en el DTO de Tipo Informe
    Ignorado en la serialización JSON en el DTO de Tipo Informe. En vez de comenzar el campo por si se quiere manipular dentro del servicio

--- AvaluoAPI/Presentation/DTOs/TipoInformeDTOs/TipoInformeDTO.cs
+++ AvaluoAPI/Presentation/DTOs/TipoInformeDTOs/TipoInformeDTO.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace AvaluoAPI.Presentation.DTOs.UserDTOs
 {
     public class TipoInformeDTO
     {
-        // Comenté el ID porque la base de datos lo autogenera, así que no necesito enviarlo desde el API
-        // public int Id { get; set; } 
+        [JsonIgnore]
+        public int Id { get; set; } 
+
         [Required(ErrorMessage = "La descripción del tipo de informe es requerida")]
         [StringLength(255, ErrorMessage = "La descripción no puede tener más de 255 caracteres.")]
         public string Descripcion { get; set; }
