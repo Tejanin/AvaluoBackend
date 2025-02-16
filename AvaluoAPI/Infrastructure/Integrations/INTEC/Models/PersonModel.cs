8f507864eb784cf7f2b97a792796b33d34c5e69d
1106278 <1106278@est.intec.edu.do>
2025-02-16 16:44:08 -0400

    Implementando el servicio mock de INTEC

---
+++ AvaluoAPI/Infrastructure/Integrations/INTEC/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿namespace AvaluoAPI.Infrastructure.Integrations.INTEC.Models
+{
+    public class PersonModel
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cargo { get; set; }
+
+    }
+
+    public class  EstudianteModel : PersonModel
+    {
+        
+    }
+
+    public class ProfesorModel : PersonModel
+    {
+        public List<SeccionModel>? Secciones { get; set; }
+
+    }
+
+
+}
