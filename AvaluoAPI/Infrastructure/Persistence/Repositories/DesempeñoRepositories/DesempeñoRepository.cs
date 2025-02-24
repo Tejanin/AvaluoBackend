a02d56a356bebec801234e6df1edc8aa14325896
1106278 <1106278@est.intec.edu.do>
2025-02-24 15:25:51 -0400

    Agregando algunos repositories

---
+++ AvaluoAPI/Infrastructure/Persistence/Repositories/DesempeñoRepositories/DesempeñoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Avaluo.Infrastructure.Data;
+using Avaluo.Infrastructure.Data.Models;
+using Avaluo.Infrastructure.Persistence.Repositories.Base;
+using AvaluoAPI.Infrastructure.Data.Contexts;
+
+namespace AvaluoAPI.Infrastructure.Persistence.Repositories.IDesempeñoRepositories
+{
+    public class DesempeñoRepository: Repository<Desempeno>, IDesempeñoRepository
+    {
+        private readonly DapperContext _dapperContext;
+        public DesempeñoRepository(AvaluoDbContext context, DapperContext dapperContext): base(context)
+        {
+            _dapperContext= dapperContext;
+        }
+
+        public AvaluoDbContext? AvaluoDbContext
+        {
+            get { return _context as AvaluoDbContext; }
+        }
+    }
+}
---
+++ AvaluoAPI/Infrastructure/Persistence/Repositories/DesempeñoRepositories/IDesempeñoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Avaluo.Infrastructure.Data.Models;
+using Avaluo.Infrastructure.Persistence.Repositories.Base;
+using NuGet.Protocol.Core.Types;
+
+namespace AvaluoAPI.Infrastructure.Persistence.Repositories.IDesempeñoRepositories
+{
+    public interface IDesempeñoRepository : IRepository<Desempeno>
+    {
+    }
+}
