ac8eec275abad404339b522e65ce284ddf491fd3
rayfel <rayfelogando@gmail.com>
2025-03-23 10:57:04 -0400

    Nuevo ViewModel y métodos GetAll y GetById. Se añadieron más filtros.
    - Cambié el ViewModel de informe para que traiga la descripción del tipo y el nombre de la carrera.
    - Se agregaron los cambios a GetAllInformesAsync para usar el nuevo ViewModel, además de nuevos filtros.
    - También se cambió GetInformeByIdAsync para utilizar el nuevo ViewModel.

--- AvaluoAPI/Domain/Services/InformeService/IInformeService.cs
+++ AvaluoAPI/Domain/Services/InformeService/IInformeService.cs
@@ -7,7 +7,7 @@ namespace AvaluoAPI.Domain.Services.InformeService
 {
     public interface IInformeService
     {
-        Task<PaginatedResult<InformeViewModel>> GetAll(int? idTipo, int? idCarrera, int? año, int? trimestre, string? periodo, int? page, int? recordsPerPage);
+        Task<PaginatedResult<InformeViewModel>> GetAll(int? idTipo, int? idCarrera, string? nombre, int? año, char? trimestre, string? periodo, int? page, int? recordsPerPage);
         Task<InformeViewModel> GetById(int id);
         Task Register(InformeDTO InformeDTO);
         Task RegistrarInformeGenerado(InformeDesempeñoViewModel informe, string pdfPath);
